69afcbbe7e8df7f873fca3baa615d283b79f414e
Ryutaro Inoue <kametaro0724@hotmail.com>
2024-09-08 17:15:27 +0900

    add recover notification function

--- get_health.py
+++ get_health.py
@@ -4,13 +4,16 @@ import json
 import requests
 #import datetime
 from datetime import datetime
+from datetime import timedelta
 from pytz import timezone,utc
 from influxdb import InfluxDBClient
 
 appsetting = json.load(open('/usr/local/etc/nasu-alerter.json', 'r'))
 discord_hook_url = appsetting["discord_hook_url"]
+alerted_flag = 0
 
 def getHealth():
+	global alerted_flag
 
 	client = InfluxDBClient('localhost', 8086, '', '', 'nasu_health')
 	result = client.query('select last(temp),temp from nasu_thp limit 1;')
@@ -23,13 +26,19 @@ def getHealth():
 			#print(datetime.timestamp(l_time))
 			time_delta = datetime.timestamp(now) - datetime.timestamp(l_time) - 32400
 			#print(time_delta)
-			if time_delta > 180 and time_delta <= 600:
-				sendDiscord(l_time)
+			if time_delta > 180 and alerted_flag == 0:
+				o_time = l_time + timedelta(hours=9)
+				sendDiscord("Nasu Offline at " + o_time.strftime('%Y/%m/%d %H:%M:%S'))
+				alerted_flag = 1
+			elif time_delta <= 60 and alerted_flag == 1:
+				o_time = l_time + timedelta(hours=9)
+				sendDiscord("Nasu Recovered at " + o_time.strftime('%Y/%m/%d %H:%M:%S'))
+				alerted_flag = 0
 			break
 		break
 			#print ("\n")
-def sendDiscord(time):
-	json_body = {"content": "Nasu Offline at " + time}
+def sendDiscord(content_str):
+	json_body = {"content": content_str}
 	json_data = json.dumps(json_body)
 	headers = {"Content-Type" : "application/json","Accept":"application/json"}
 	response = requests.post(discord_hook_url,headers=headers,data=json_data,timeout=10)	
@@ -37,5 +46,4 @@ def sendDiscord(time):
 while True:
 	getHealth()
 	time.sleep(300)
-
 
